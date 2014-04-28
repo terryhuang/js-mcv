daabfa8f34f15452d1486bf09ced058d66032bb3
terryhuang <terryhuangxin@gmail.com>
2014-04-28 11:27:49 +0800

    Create js-build-profile.js

---
+++ build-tool/profiles/js-build-profile.js
@@ -0,0 +1,72 @@
+({
+
+    //By default, all modules are located relative to this path. If baseUrl
+    //is not explicitly set, then all modules are loaded relative to
+    //the directory that holds the build file. If appDir is set, then
+    //baseUrl should be specified as relative to the appDir.
+    //baseUrl: "../../src/js",
+
+    //By default all the configuration for optimization happens from the command
+    //line or by properties in the a config file, and configuration that was
+    //passed to requirejs as part of the app's runtime "main" JS file is *not*
+    //considered. However, if you prefer for the that "main" JS file configuration
+    //to be read for the build so that you do not have to duplicate the values
+    //in a separate configuration, set this property to the location of that
+    //main JS file. The first requirejs({}), require({}), requirejs.config({}),
+    //or require.config({}) call found in that file will be used.
+    mainConfigFile: '../../src/js/main.js',
+
+    //The directory path to save the output. If not specified, then
+    //the path will default to be a directory called "build" as a sibling
+    //to the build file. All relative paths are relative to the build file.
+    //dir: "build/stage",
+
+    //How to optimize all the JS files in the build output directory.
+    //Right now only the following values
+    //are supported:
+    //- "uglify": (default) uses UglifyJS to minify the code.
+    //- "closure": uses Google's Closure Compiler in simple optimization
+    //mode to minify the code. Only available if running the optimizer using
+    //Java.
+    //- "closure.keepLines": Same as closure option, but keeps line returns
+    //in the minified files.
+    //- "none": no minification will be done.
+    optimize: "uglify",
+
+    //If using Closure Compiler for script optimization, these config options
+    //can be used to configure Closure Compiler. See the documentation for
+    //Closure compiler for more information.
+    //closure: {
+    //    CompilerOptions: {},
+    //    CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+    //    loggingLevel: 'WARNING'
+    //},
+
+    keepLines: false,
+    
+    //By default, comments that have a license in them are preserved in the
+    //output. However, for a larger built files there could be a lot of
+    //comment files that may be better served by having a smaller comment
+    //at the top of the file that points to the list of all the licenses.
+    //This option will turn off the auto-preservation, but you will need
+    //work out how best to surface the license information.
+    preserveLicenseComments: true,
+
+    //If you only intend to optimize a module (and its dependencies), with
+    //a single file as the output, you can specify the module options inline,
+    //instead of using the 'modules' section above. 'exclude',
+    //'excludeShallow', 'include' and 'insertRequire' are all allowed as siblings
+    //to name. The name of the optimized file is specified by 'out'.
+    name: "main",
+    out: "../../src/js/main-min.js",
+
+    //Sets the logging level. It is a number. If you want "silent" running,
+    //set logLevel to 4. From the logger.js file:
+    //TRACE: 0,
+    //INFO: 1,
+    //WARN: 2,
+    //ERROR: 3,
+    //SILENT: 4
+    //Default is 0.
+    logLevel: 1,
+})
